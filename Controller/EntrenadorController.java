4a83a536b3103f1af45179c8e37abcf15bd56f2e
ibisayGutierrez <ibgutierrezal@est.utn.ac.cr>
2024-12-01 22:15:22 -0600

    Se crea la View

--- Controller/EntrenadorController.java
+++ Controller/EntrenadorController.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Contoller;
+package Controller;
 
 /**
  *
  * @author Dell
  */
-public class ClienteController {
+public class EntrenadorController {
     
 }
